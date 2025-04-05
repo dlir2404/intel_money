54e7690b73cbdc0382af7ceb9ddcab00277e5958
Dlir <dinhlinh.work24@gmail.com>
2025-04-05 20:53:54 +0700

    sf update category store

--- lib/core/state/app_state.dart
+++ lib/core/state/app_state.dart
@@ -12,12 +12,14 @@ class AppState extends ChangeNotifier {
   List<Category> _expenseCategories = [];
   List<Category> _incomeCategories = [];
 
-
-
   User? get user => _user;
+
   List<Wallet> get wallets => _wallets;
+
   List<Category> get categories => _categories;
+
   List<Category> get expenseCategories => _expenseCategories;
+
   List<Category> get incomeCategories => _incomeCategories;
 
   void setUser(User user) {
@@ -48,30 +50,64 @@ class AppState extends ChangeNotifier {
 
   void setCategories(List<Category> categories) {
     _categories = categories;
-    _expenseCategories = categories.where((element) => element.type == CategoryType.expense).toList();
-    _incomeCategories = categories.where((element) => element.type == CategoryType.income).toList();
+
+    //reference variables
+    _expenseCategories =
+        categories
+            .where((element) => element.type == CategoryType.expense)
+            .toList();
+    _incomeCategories =
+        categories
+            .where((element) => element.type == CategoryType.income)
+            .toList();
     notifyListeners();
   }
 
   void addCategory(Category category) {
-    _categories.add(category);
-    if (category.type == CategoryType.expense) {
-      _expenseCategories.add(category);
+    if (category.parentId == null) {
+      _categories.add(category);
+      if (category.type == CategoryType.expense) {
+        _expenseCategories.add(category);
+      } else {
+        _incomeCategories.add(category);
+      }
     } else {
-      _incomeCategories.add(category);
+      final parent = _categories.firstWhere(
+        (element) => element.id == category.parentId,
+      );
+      parent.addChild(category);
+      //NOTICE: cause income & expense categories just reference variables, we do not need to add once again
+      //The code below is wrong
+      // if (category.type == CategoryType.expense) {
+      //   final expenseParent = _expenseCategories.firstWhere(
+      //     (element) => element.id == category.parentId,
+      //   );
+      //   expenseParent.addChild(category);
+      // } else {
+      //   final incomeParent = _incomeCategories.firstWhere(
+      //     (element) => element.id == category.parentId,
+      //   );
+      //   incomeParent.addChild(category);
+      // }
     }
     notifyListeners();
   }
 
   void updateCategory(Category category) {
-    final index = _categories.indexWhere((element) => element.id == category.id);
+    final index = _categories.indexWhere(
+      (element) => element.id == category.id,
+    );
     _categories[index] = category;
 
     if (category.type == CategoryType.expense) {
-      final index = _expenseCategories.indexWhere((element) => element.id == category.id);
+      final index = _expenseCategories.indexWhere(
+        (element) => element.id == category.id,
+      );
       _expenseCategories[index] = category;
     } else {
-      final index = _incomeCategories.indexWhere((element) => element.id == category.id);
+      final index = _incomeCategories.indexWhere(
+        (element) => element.id == category.id,
+      );
       _incomeCategories[index] = category;
     }
     notifyListeners();
