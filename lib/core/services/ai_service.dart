0f9765ae7a5baf1a7b646c3dc496ad7dad1249d4
Dlir <dinhlinh.work24@gmail.com>
2025-05-22 23:14:56 +0700

    update cached when create trans with ai chat

--- lib/core/services/ai_service.dart
+++ lib/core/services/ai_service.dart
@@ -1,11 +1,20 @@
 import 'package:intel_money/core/models/extract_transaction_info_response.dart';
 
+import '../../shared/const/enum/transaction_type.dart';
 import '../models/message.dart';
 import '../models/transaction.dart';
 import '../network/api_client.dart';
+import '../state/app_state.dart';
+import '../state/statistic_state.dart';
+import '../state/transaction_state.dart';
+import '../state/wallet_state.dart';
 
 class AIService {
   final ApiClient _apiClient = ApiClient.instance;
+  final TransactionState _transactionState = TransactionState();
+  final WalletState _walletState = WalletState();
+  final AppState _appState = AppState();
+  final StatisticState _statisticState = StatisticState();
 
   static final AIService _instance = AIService._internal();
   factory AIService() => _instance;
@@ -34,6 +43,19 @@ class AIService {
       transactions = (response['transactions'] as List)
           .map((transaction) => Transaction.fromJson(transaction))
           .toList();
+    }
+
+    for (var transaction in transactions) {
+      _transactionState.addTransaction(transaction);
+      if (transaction.type == TransactionType.expense) {
+        _appState.decreaseUserBalance(transaction.amount);
+        _walletState.decreateWalletBalance(transaction.sourceWallet.id, transaction.amount);
+        _statisticState.updateStatisticData(transaction);
+      } else if (transaction.type == TransactionType.income) {
+        _appState.increaseUserBalance(transaction.amount);
+        _walletState.increaseWalletBalance(transaction.sourceWallet.id, transaction.amount);
+        _statisticState.updateStatisticData(transaction);
+      }
     }
 
     final Message message = Message.newAgentMessage(
